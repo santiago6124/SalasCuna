013bbea957142d4b4254484bbc90113d54126572
facuolivamar <facundoolivam@gmail.com>
2023-04-27 08:21:24 -0300

    models review

--- dataMigrate/data/models.py
+++ dataMigrate/data/models.py
@@ -45,11 +45,11 @@ class TipoCaracteristicaTel(models.Model):
     caracteristica = models.IntegerField()
 
     def __str__(self):
-        return self.caracteristica
+        return f"{self.caracteristica}"
 
 class TipoBarrio(models.Model):
     # barrio de chico en sala cuna
-    barrio = models.IntegerField()
+    barrio = models.CharField(max_length=100)
 
     def __str__(self):
         return self.barrio
@@ -64,18 +64,23 @@ class Madre(models.Model):
         return f"{self.apellido_y_nombre}"
 
 class Chico(models.Model):
+    dni = models.IntegerField(primary_key=True)
+    sala_cuna = models.ForeignKey(SalaCuna, on_delete=models.CASCADE )
     apellido = models.CharField(max_length=40)
     nombre = models.CharField(max_length=40)
+    fecha_nacimiento = models.DateField()
+    sexo = models.ForeignKey(TipoSexo, on_delete=models.CASCADE )
     calle = models.CharField(max_length=40)
     numero_calle = models.IntegerField()
-    numero_telefono = models.IntegerField()
-    dni = models.IntegerField()
-    fecha_nacimiento = models.DateField()
     barrio = models.ForeignKey( TipoBarrio, on_delete=models.CASCADE )
     localidad = models.ForeignKey(TipoLocalidad, on_delete=models.CASCADE )
     estado = models.ForeignKey(TipoEstado, on_delete=models.CASCADE )
     turno = models.ForeignKey(TipoTurno, on_delete=models.CASCADE )
-    sexo = models.ForeignKey(TipoSexo, on_delete=models.CASCADE )
+    caracteristica = models.ForeignKey(TipoCaracteristicaTel, on_delete=models.CASCADE )
+    numero_telefono = models.IntegerField()
     madre = models.ForeignKey(Madre, on_delete=models.CASCADE )
-    sala_cuna = models.ForeignKey(SalaCuna, on_delete=models.CASCADE )
+    
+    def __str__(self):
+        return f"{self.apellido} {self.nombre} {self.calle} {self.numero_calle} {self.numero_telefono} {self.dni} {self.fecha_nacimiento} {self.sala_cuna} {self.estado} {self.turno} {self.madre} {self.sexo} {self.localidad} {self.barrio}   "
 
+# manage.py runserver
