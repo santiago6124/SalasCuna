481a31a531797a45b0639a17bfdc8e01ec9bbbcb
Lichi <lichimarquez04@gmail.com>
2023-05-09 10:38:44 -0300

    UI changed

--- SalaCuna/confApp/admin.py
+++ SalaCuna/confApp/admin.py
@@ -2,16 +2,28 @@ from django.contrib import admin
 
 # Register your models here.
 
-#from .models import *
-#admin.site.register(Addresses)
-#admin.site.register()
-
-from django.apps import apps
-
-all_models = apps.get_models()
-
-for model in all_models:
-    try:
-        admin.site.register(model)
-    except admin.sites.AlreadyRegistered:
-        pass
+from .models import *
+admin.site.register(Addresses)
+admin.site.register(Answers)
+admin.site.register(Children)
+admin.site.register(ChildrenState)
+admin.site.register(Cribrooms)
+admin.site.register(Departments)
+admin.site.register(Desinfections)
+admin.site.register(Districts)
+admin.site.register(Forms)
+admin.site.register(Genders)
+admin.site.register(GuardianPhones)
+admin.site.register(Guardians)
+admin.site.register(Localities)
+admin.site.register(Options)
+admin.site.register(Padrones)
+admin.site.register(Paynote)
+admin.site.register(Payouts)
+admin.site.register(QuestionTypes)
+admin.site.register(Questions)
+admin.site.register(Roles)
+admin.site.register(Shifts)
+admin.site.register(UserEmails)
+admin.site.register(Users)
+admin.site.register(Zones)
