e9b517fca25b51c9bcece125ecfd59ea67f49228
Franco-Ball <francoball181@gmail.com>
2023-05-11 10:23:51 -0300

    user models & settings

--- SalaCuna/SalaCuna/settings.py
+++ SalaCuna/SalaCuna/settings.py
@@ -240,3 +240,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "confApp.UserAccount"
