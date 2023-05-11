7302a46b9c101356dc043d0bb35ea77930a57991
Franco-Ball <francoball181@gmail.com>
2023-05-11 11:58:56 -0300

    work in progress

--- SalaCuna/SalaCuna/settings.py
+++ SalaCuna/SalaCuna/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -169,7 +170,7 @@ ROOT_URLCONF = "SalaCuna.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'build')],  
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -192,8 +193,8 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "SalasCunas",
-        "USER": "djangoProject",
-        "PASSWORD": "1234",
+        "USER": "franco",
+        "PASSWORD": "pepe1234",
         "HOST": "localhost",
         "PORT": "3306",
     }
@@ -235,6 +236,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'build/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- SalaCuna/confApp/models.py
+++ SalaCuna/confApp/models.py
@@ -23,7 +23,7 @@ class UserAccountManager(BaseUserManager):
         
 
 class UserAccount(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(max_lenght=255, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=True)
