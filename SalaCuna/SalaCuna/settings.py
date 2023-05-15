8935ed64af28b73b7e88752aea647f61446ab4a8
Franco Ball <francoball181@gmail.com>
2023-05-15 17:49:43 -0300

    Auth permission class in rest framework

--- SalaCuna/SalaCuna/settings.py
+++ SalaCuna/SalaCuna/settings.py
@@ -245,6 +245,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # JWT Auth
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated'
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         (...)
