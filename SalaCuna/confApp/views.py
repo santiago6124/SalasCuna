65cfd6c3596223442b1eed209f5d8eed9b3cc7c1
facuolivamar <facundoolivam@gmail.com>
2023-05-16 10:03:29 -0300

    importing serializers to view and initializing classes

--- SalaCuna/confApp/views.py
+++ SalaCuna/confApp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import generics
+from .models import Children, Guardians, Addresses
+from .serializers import ChildrenSerializer, GuardiansSerializer, AddressesSerializer
+
+class ChildrenListCreateView(generics.ListCreateAPIView):
+    queryset = Children.objects.all()
+    serializer_class = ChildrenSerializer
+
+class ChildrenRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Children.objects.all()
+    serializer_class = ChildrenSerializer
+
+class GuardiansListCreateView(generics.ListCreateAPIView):
+    queryset = Guardians.objects.all()
+    serializer_class = GuardiansSerializer
+
+class AddressesListCreateView(generics.ListCreateAPIView):
+    queryset = Addresses.objects.all()
+    serializer_class = AddressesSerializer
