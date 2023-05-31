5988cecdd40c216952a88988a2557bd1449051b4
Franco-Ball <francoball181@gmail.com>
2023-05-31 09:43:56 -0300

    change token lifetime

--- SalaCuna/SalaCuna/settings.py
+++ SalaCuna/SalaCuna/settings.py
@@ -252,7 +252,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'AUTH_TOKEN_CLASSES' : (
         'rest_framework_simplejwt.tokens.AccessToken',
