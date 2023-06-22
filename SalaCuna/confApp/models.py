e684e6c284d3f7342a11931196807a8ac18fddde
Franco-Ball <francoball181@gmail.com>
2023-06-22 20:43:40 -0300

    starting to implement custom user in models

--- SalaCuna/confApp/models.py
+++ SalaCuna/confApp/models.py
@@ -20,6 +20,18 @@ class UserAccountManager(BaseUserManager):
         user.save()
 
         return user
+
+    """def create_superuser(self, email, password=None, is_superuser=True ,**extra_fields):
+        if not email:
+            raise ValueError("Users must have an email address")
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+
+        user.set_password(password)
+        user.save()
+
+        return user"""
         
 
 class UserAccount(AbstractBaseUser, PermissionsMixin):
