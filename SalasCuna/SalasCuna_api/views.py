5b1e7c1c247c9e2915e2fd79a50c1cb1834b7304
facuolivamar <facundoolivam@gmail.com>
2023-07-31 02:02:43 -0300

    child update method

--- SalasCuna/SalasCuna_api/views.py
+++ SalasCuna/SalasCuna_api/views.py
@@ -36,3 +36,32 @@ class ChildModelViewSet(viewsets.ModelViewSet):
     queryset = Child.objects.all()
     serializer_class = ChildSerializer
     permission_classes = [AllowAny]
+    
+
+    
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        disenroll = bool(self.request.query_params.get('disenroll'))
+        # disenroll_date
+        print(f'paramter: {disenroll}')
+        
+        if disenroll == True:
+            print(f'disenroll: {disenroll}')
+            instance.disenroll_date = datetime.now()
+            # instance.user=self.request.user
+            instance.save()
+
+            return Response( status=status.HTTP_202_ACCEPTED)
+        else: 
+            # If 'disenroll' parameter is not present, proceed with normal update
+            serializer = self.get_serializer(instance, data=request.data, partial=True)
+
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
