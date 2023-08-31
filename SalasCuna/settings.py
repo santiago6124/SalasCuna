66a69c63452273b84209b69eab2f731635abf69b
facuolivamar <facundoolivam@gmail.com>
2023-08-31 00:32:44 -0300

    some settings mod

--- SalasCuna/settings.py
+++ SalasCuna/settings.py
@@ -52,6 +52,7 @@ JAZZMIN_UI_TWEAKS = JAZZMIN_UI_TWEAKS
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -133,7 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'build/static'), )
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'build/static'), )
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR/ 'build/static'
 # JWT Authentication
 
 REST_FRAMEWORK = {
