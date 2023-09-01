123bf2c693c302ef7a164bd18094e13a39821ae6
santiago6124 <71385552+santiago6124@users.noreply.github.com>
2023-09-01 18:45:00 -0300

    Update settings.py

--- SalasCuna/settings.py
+++ SalasCuna/settings.py
@@ -16,10 +16,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 1
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['salascuna.azurewebsites.net']
-SECURE_SSL_REDIRECT=0
 
 # Application definition
 
