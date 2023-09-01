9e5479ff1a58e2db29a99c481c3bb9f3138f8157
facuolivamar <facundoolivam@gmail.com>
2023-09-01 16:39:31 -0300

    1st custom Celery task

--- SalasCuna/SalasCuna_api/tasks.py
+++ SalasCuna/SalasCuna_api/tasks.py
@@ -1,13 +1,13 @@
 # Create your tasks here
 
-from SalasCuna_api.models import Widget
+from .models import Child
 
 from celery import shared_task
 
 
 @shared_task
-def add(x, y):
-    return x + y
+def getChild(child_id):
+    return Child.objects.all().get(id = int(child_id)).dni
 
 
 @shared_task
@@ -15,18 +15,18 @@ def mul(x, y):
     return x * y
 
 
-@shared_task
-def xsum(numbers):
-    return sum(numbers)
+# @shared_task
+# def xsum(numbers):
+#     return sum(numbers)
 
 
-@shared_task
-def count_widgets():
-    return Widget.objects.count()
+# @shared_task
+# def count_widgets():
+#     return Widget.objects.count()
 
 
-@shared_task
-def rename_widget(widget_id, name):
-    w = Widget.objects.get(id=widget_id)
-    w.name = name
-    w.save()
+# @shared_task
+# def rename_widget(widget_id, name):
+#     w = Widget.objects.get(id=widget_id)
+#     w.name = name
+#     w.save()
