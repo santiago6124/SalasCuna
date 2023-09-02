128cfed9e1c6935e89a3c95ef92655f6a8e7be7c
Lichi <lichimarquez04@gmail.com>
2023-09-02 16:22:11 -0300

    All roles perms created

--- SalasCuna/SalasCuna_api/permissions.py
+++ SalasCuna/SalasCuna_api/permissions.py
@@ -33,3 +33,71 @@ class TrabajadorSocialPerms(permissions.BasePermission):
             return True
         else:
             return False
+
+
+class ArquitectoPerms(permissions.BasePermission):
+    message = "Solo las personas con rol Arquitecto pueden acceder a esta información"
+
+    def has_permission(self, request, view):
+        if request.user.groups.filter(name="Arquitecto").exists():
+            return request.method
+        else:
+            return False
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.groups.filter(name="Arquitecto").exists():
+            return True
+        else:
+            return False
+
+
+class PsicopedagogaPerms(permissions.BasePermission):
+    message = (
+        "Solo las personas con rol Psicopedagoga pueden acceder a esta información"
+    )
+
+    def has_permission(self, request, view):
+        if request.user.groups.filter(name="Psicopedagoga").exists():
+            return request.method
+        else:
+            return False
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.groups.filter(name="Psicopedagoga").exists():
+            return True
+        else:
+            return False
+
+
+class AdministradorPerms(permissions.BasePermission):
+    message = (
+        "Solo las personas con rol Administrador pueden acceder a esta información"
+    )
+
+    def has_permission(self, request, view):
+        if request.user.groups.filter(name="Administrador").exists():
+            return request.method
+        else:
+            return False
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.groups.filter(name="Administrador").exists():
+            return True
+        else:
+            return False
+
+
+class SecretarioPerms(permissions.BasePermission):
+    message = "Solo las personas con rol Secretario pueden acceder a esta información"
+
+    def has_permission(self, request, view):
+        if request.user.groups.filter(name="Secretario").exists():
+            return request.method
+        else:
+            return False
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.groups.filter(name="Secretario").exists():
+            return True
+        else:
+            return False
