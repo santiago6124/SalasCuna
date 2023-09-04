42e77a952afd54f5e6ca4e26105ff17dcd492cd4
santiago6124 <71385552+santiago6124@users.noreply.github.com>
2023-09-04 20:00:09 -0300

    Update settings.py

--- SalasCuna/settings.py
+++ SalasCuna/settings.py
@@ -16,9 +16,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = False
 
-ALLOWED_HOSTS = ['salascuna1.azurewebsites.net']
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 
