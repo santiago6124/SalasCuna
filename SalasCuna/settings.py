7d283ebd725cce1f5845755bfaaab243d0e00e03
santiago6124 <santiagocarranzazinny@gmail.com>
2023-09-06 10:57:23 -0300

    test

--- SalasCuna/settings.py
+++ SalasCuna/settings.py
@@ -15,6 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
 
