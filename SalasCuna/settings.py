a1c2da23d99d76af3e31ecebc206a018d4c24fd8
santiago6124 <santiagocarranzazinny@gmail.com>
2023-09-07 08:20:39 -0300

    delete whitenoise static 138

--- SalasCuna/settings.py
+++ SalasCuna/settings.py
@@ -136,7 +136,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = "/static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'build/static'), )
 # JWT Authentication
