3f78207f27f5793c802384cc49ac05b2e2c36e2c
facuolivamar <facundoolivam@gmail.com>
2023-09-14 18:43:12 -0300

    models 1st part

--- SalasCuna/Encuesta/admin.py
+++ SalasCuna/Encuesta/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import *
+
+
+class EncuestaAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    list_filter = ["name"]
+
+
+class TipoPreguntaAdmin(admin.ModelAdmin):
+    list_display = ("id", "tipoPregunta")
+    list_filter = ["tipoPregunta"]
+
+class PreguntaAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "parentQuestion", "tipoPregunta")
+    list_filter = ["name", "parentQuestion", "tipoPregunta"]
+
+class TipoRepuestaAdmin(admin.ModelAdmin):
+    list_display = ("id", "tipoRepuesta")
+    list_filter = ["tipoRepuesta"]
+
+class RespuestaAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "tipoRespuesta")
+    list_filter = ["name", "tipoRespuesta"]
