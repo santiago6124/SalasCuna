de7880422cc303283b94ee57038e62d8ecf4714c
santiago6124 <santiagocarranzazinny@gmail.com>
2023-09-19 09:30:23 -0300

    added build settings urls and views

--- SalasCuna/urls.py
+++ SalasCuna/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
     path("api/", include("SalasCuna_api.urls")),
+    #path("excel-to-db/", include("excel_to_db.urls")),
+    re_path(r"^(?:.*)?$", TemplateView.as_view(template_name="index.html"))
 ]
-
-urlpatterns += [re_path(r"^.*", TemplateView.as_view(template_name="index.html"))]
