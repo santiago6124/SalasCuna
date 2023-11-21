a574b7a66254a5ff1f0a2a912be3f57749ebe59d
Lichi <lichimarquez04@gmail.com>
2023-11-21 09:56:32 -0300

    documentation in docs/ and download .yaml

--- SalasCuna/SalasCuna/urls.py
+++ SalasCuna/SalasCuna/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
-from rest_framework.documentation import include_docs_urls
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
     path("api/", include("SalasCuna_api.urls")),
-    path('docs/', include_docs_urls(title='My API title', public=False)),
+    path('docs/download', SpectacularAPIView.as_view(), name='schema'),
+    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
 
 urlpatterns += [re_path(r"^.*", TemplateView.as_view(template_name="index.html"))]
