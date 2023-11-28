34dbeb293e7b09f7ac2357f65d3851c6179d7cc5
santiago6124 <santiagocarranzazinny@gmail.com>
2023-11-27 22:38:33 -0300

    build displayed

--- SalasCuna/settings.py
+++ SalasCuna/settings.py
@@ -16,8 +16,7 @@ SECRET_KEY = "django-insecure-5f-+9^y8sdrrxk!93+h)h7#5(h-#o8(ypykyjs-t764u3(7_0#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -64,7 +63,7 @@ ROOT_URLCONF = "SalasCuna.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "build")],
+        "DIRS": [os.path.join(BASE_DIR, "frontend/build")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -130,7 +129,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/build/static'), )
+STATIC_ROOT = BASE_DIR/ 'frontend/build/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # JWT Authentication
 
--- SalasCuna/urls.py
+++ SalasCuna/urls.py
@@ -1,15 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework.documentation import include_docs_urls
+from django.conf import settings # new
+from  django.conf.urls.static import static #new
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
     path("api/", include("SalasCuna_api.urls")),
-    path('docs/download', SpectacularAPIView.as_view(), name='schema'),
-    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('docs/', include_docs_urls(title='My API title', public=False)),
+    re_path(r"^(?:.*)?$", TemplateView.as_view(template_name="index.html"))
 ]
 
-urlpatterns += [re_path(r"^.*", TemplateView.as_view(template_name="index.html"))]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
