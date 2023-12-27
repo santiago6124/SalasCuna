046ad8138a3bef47c4151b5f15b5dd6f0e0d3f7e
santiago6124 <santiagocarranzazinny@gmail.com>
2023-12-27 12:54:05 -0300

    build displayed

--- SalasCuna/settings.py
+++ SalasCuna/settings.py
@@ -16,8 +16,7 @@ SECRET_KEY = "django-insecure-5f-+9^y8sdrrxk!93+h)h7#5(h-#o8(ypykyjs-t764u3(7_0#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -61,10 +60,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "SalasCuna.urls"
 
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "build")],
+        "DIRS": [os.path.join(BASE_DIR, "frontend/build")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -126,7 +126,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/build/static'), )
+STATIC_ROOT = BASE_DIR/ 'frontend/build/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # JWT Authentication
 
